4ec69b7cc9f26d116e5f04e07bd15949257697e5
Michael Cuomo <mcuomo777@gmail.com>
2025-11-22 00:56:06 -0500

    adding interface and some logic for primitive, both render and physics

--- main.go
+++ main.go
@@ -3,12 +3,69 @@ package main
 import (
 	"fmt"
 
+	"github.com/gopxl/pixel/v2"
+	"github.com/gopxl/pixel/v2/backends/opengl"
+	"github.com/gopxl/pixel/v2/ext/imdraw"
 	"github.com/mykeelium/visual-playground/collatz"
+	"github.com/mykeelium/visual-playground/primitives"
+	"golang.org/x/image/colornames"
 )
 
 func main() {
-	tree := collatz.BuildTree(100000)
+	tree := collatz.BuildTree(100)
 
 	fmt.Println("tree:")
 	collatz.PrintOrganicTree(&tree)
+	opengl.Run(run)
+}
+
+func drawEntities(imd *imdraw.IMDraw, entities []*primitives.Entity) {
+	for _, e := range entities {
+		e.Draw(imd)
+	}
+}
+
+func updateEntities(entities []*primitives.Entity) {
+	for _, e := range entities {
+		e.Update(float64(1) / 30)
+	}
+}
+
+func run() {
+	var width float64 = 1024
+	var height float64 = 768
+	cfg := opengl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, width, height),
+		VSync:  true,
+	}
+
+	win, err := opengl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	imd := imdraw.New(nil)
+	circles := []*primitives.Entity{}
+
+	var startX float64 = 400
+	for range 10 {
+		var startY float64 = 750
+		for range 2 {
+			circles = append(circles, primitives.NewCircleEntity(startX, startY, 10, 0, 1, 0, 0))
+		}
+		startX += 40
+	}
+
+	gravity := primitives.Float2{X: 0, Y: -9.8}
+
+	for !win.Closed() {
+		imd.Clear()
+		win.Clear(colornames.Black)
+		primitives.ApplyGravity(circles, gravity)
+		updateEntities(circles)
+		drawEntities(imd, circles)
+		imd.Draw(win)
+		win.Update()
+	}
 }
---
+++ primitives/fluid.go
@@ -0,0 +1,115 @@
+package primitives
+
+import (
+	"github.com/gopxl/pixel/v2"
+	"github.com/gopxl/pixel/v2/ext/imdraw"
+)
+
+var (
+	ZeroFloat2 = Float2{0, 0}
+	ZeroFloat3 = Float3{0, 0, 0}
+)
+
+type Color struct {
+	Red   float64
+	Green float64
+	Blue  float64
+}
+
+type Float3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func (v Float3) Add(o Float3) Float3 {
+	return Float3{v.X + o.X, v.Y + o.Y, v.Z + o.Z}
+}
+
+func (v Float3) Scale(f float64) Float3 {
+	return Float3{v.X * f, v.Y * f, v.Z * f}
+}
+
+// Float2 is Used to handle position, motion, and force in two dimensions
+type Float2 struct {
+	X float64
+	Y float64
+}
+
+func (v Float2) Add(o Float2) Float2 {
+	return Float2{v.X + o.X, v.Y + o.Y}
+}
+
+func (v Float2) Scale(f float64) Float2 {
+	return Float2{v.X * f, v.Y * f}
+}
+
+// The PhysicsComponent contains information for running the physics calculations for an object
+type PhysicsComponent struct {
+	Position     Float2
+	Velocity     Float2
+	Acceleration Float2
+	Mass         float64
+}
+
+func (p *PhysicsComponent) ApplyForce(force Float2) {
+	if p.Mass != 0 {
+		p.Acceleration = p.Acceleration.Add(force.Scale(1 / p.Mass))
+	}
+}
+
+func (p *PhysicsComponent) Integrate(dt float64) {
+	p.Velocity = p.Velocity.Add(p.Acceleration.Scale(dt))
+	p.Position = p.Position.Add(p.Velocity.Scale(dt))
+	p.Acceleration = ZeroFloat2
+}
+
+// The RenderComponet holds the logic for rendering objects
+type RenderComponet interface {
+	Draw(imd *imdraw.IMDraw, position pixel.Vec)
+}
+
+type CircleRender struct {
+	Radius    float64
+	Thickness float64
+	Color     Color
+}
+
+func (c *CircleRender) Draw(imd *imdraw.IMDraw, position pixel.Vec) {
+	imd.Color = pixel.RGB(c.Color.Red, c.Color.Green, c.Color.Blue)
+	imd.Push(position)
+	imd.Circle(c.Radius, c.Thickness)
+}
+
+// The Entity is a marrying of a PhysicsComponent and RenderComponent
+type Entity struct {
+	Physics *PhysicsComponent
+	Render  RenderComponet
+}
+
+func (e *Entity) Draw(imd *imdraw.IMDraw) {
+	e.Render.Draw(imd, pixel.Vec{X: e.Physics.Position.X, Y: e.Physics.Position.Y})
+}
+
+func (e *Entity) Update(dt float64) {
+	e.Physics.Integrate(dt)
+}
+
+func NewCircleEntity(x, y, radius, thickness float64, r, g, b float64) *Entity {
+	physics := &PhysicsComponent{
+		Position: Float2{X: x, Y: y},
+		Mass:     1.0,
+	}
+	render := &CircleRender{
+		Radius:    radius,
+		Thickness: thickness,
+		Color:     Color{Red: r, Green: g, Blue: b},
+	}
+	return &Entity{Physics: physics, Render: render}
+}
+
+func ApplyGravity(e []*Entity, g Float2) {
+	for _, ent := range e {
+		ent.Physics.ApplyForce(g.Scale(ent.Physics.Mass))
+	}
+}
